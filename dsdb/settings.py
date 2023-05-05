c964a50344c8984b791dced0003745174c2b0d1e
amir <as.daoudi@esi-sba.dz>
2023-05-05 15:56:00 +0100

    starting prod prep.

--- dsdb/settings.py
+++ dsdb/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -105,7 +106,7 @@ DATABASES = {
         'NAME': 'backend',
         'HOST' : 'localhost',#docker
         'USER' : 'root',
-        'PASSWORD' : 'Be-Strange150902'
+        'PASSWORD' : 'spartaamir12342002'
     }
 }
 
@@ -145,6 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -195,7 +197,8 @@ DEFAULT_FROM_EMAIL = 'DSDB@esi-sba.dz'
 """
 ADMINS = [
     ('as.daoudi', 'as.daoudi@esi-sba.dz'),
-]"""
+]
+"""
 
 
 SITE_NAME = "DS-DB"
@@ -346,5 +349,34 @@ JAZZMIN_UI_TWEAKS = {
         "success": "btn-success"
     },
     "actions_sticky_top": False
+}
+
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers':{
+        'console': {
+            'class': 'logging.StreamHandler'
+        },
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': 'general.log',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+        }
+    },
+    'formatter': {
+        'verbose': {
+            'format': '{asctime} ({levelname}) - {name} - {message}',
+            'style': '{',
+        }
+    }
 }
 
