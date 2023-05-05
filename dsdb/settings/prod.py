5b436cbb2293482372b7305212716b448e1eb376
amir <as.daoudi@esi-sba.dz>
2023-05-05 19:35:44 +0100

    vercel env conf.

--- dsdb/settings/prod.py
+++ dsdb/settings/prod.py
@@ -10,3 +10,14 @@ SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = ['*']
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'URL': os.getenv('MYSQL_URL'),
+        'NAME': os.getenv('MYSQLDATABASE'),
+        'USER' : os.getenv('MYSQLUSER'),
+        'PASSWORD' : os.getenv('MYSQLPASSWORD'),
+        'HOST' : os.getenv('containers-us-west-161.railway.app'),
+        'PORT' : os.getenv('MYSQLPORT')
+    }
+}
